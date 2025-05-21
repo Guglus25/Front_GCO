ebbf45a01d07b088583a562c84a1524dabdbf4a0
Cristian Felipe Jaramillo Medina <crisfel25@hotmail.com>
2025-05-20 21:54:07 -0500

    Front del aplicativo GCO

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html'
+  
+})
+export class AppComponent {
+   constructor(private modalService: NgbModal) {
+
+  }
+
+  public open(modal: any): void {
+
+    this.modalService.open(modal);
+
+  }
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { ProductPageComponent } from './pages/product/product-page.component';
+
+export const routes: Routes = [
+{
+    path: '',
+    component: ProductPageComponent,
+},
+
+];
